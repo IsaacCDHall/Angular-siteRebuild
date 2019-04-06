5fcf89d30db2c87c48aff45e397237aa01f19d63
Isaac Hall <isaacdehall@gmail.com>
2019-04-05 17:10:12 -0700

    simple google login with console to show user, update readme

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
-import { Routes, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -16,17 +15,17 @@ export class LogInComponent implements OnInit {
   private isLoggedIn: Boolean;
   private userName: String;
 
-  constructor(public authService: AuthenticationService, private router: Router) {
+  constructor(public authService: AuthenticationService) {
     this.authService.user.subscribe(user =>  {
       if (user == null) {
         this.isLoggedIn = false;
-        this.router.navigate(['login']);
+        console.log("Ya signed out")
       } else {
         this.isLoggedIn = true;
         this.userName = user.displayName;
-        console.log(this.userName);
+        console.log(`${this.userName} is signed in`);
+        console.log(user);
       }
-      console.log(user)
     });
   }
 
@@ -38,3 +37,45 @@ export class LogInComponent implements OnInit {
     this.authService.logout();
   }
 }
+// below is the updated with router
+
+// import { Component, OnInit } from '@angular/core';
+// import { AuthenticationService } from '../authentication.service';
+// import { Routes, RouterModule } from '@angular/router';
+//
+// @Component({
+//   selector: 'app-log-in',
+//   templateUrl: './log-in.component.html',
+//   styleUrls: ['./log-in.component.scss'],
+//   providers: [AuthenticationService]
+// })
+// export class LogInComponent implements OnInit {
+//
+//   ngOnInit() {
+//   }
+//   user;
+//   private isLoggedIn: Boolean;
+//   private userName: String;
+//
+//   constructor(public authService: AuthenticationService) {
+//     this.authService.user.subscribe(user =>  {
+//       if (user == null) {
+//         this.isLoggedIn = false;
+//         this.router.navigate(['login']);
+//       } else {
+//         this.isLoggedIn = true;
+//         this.userName = user.displayName;
+//         console.log(this.userName);
+//       }
+//       console.log(user)
+//     });
+//   }
+//
+//   login() {
+//     this.authService.login();
+//   }
+//
+//   logout() {
+//     this.authService.logout();
+//   }
+// }
